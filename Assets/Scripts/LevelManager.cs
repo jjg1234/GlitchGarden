3ce2d15903c2d269345dd36522a4c427eeea4ca5
JJG <jjeromegerstch@gmail.com>
2017-03-20 14:18:30 +0100

    7-133 ended
    Menu and music system OK

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This class allows us to handle Levels (load,...)
+/// </summary>
+public class LevelManager : MonoBehaviour {
+	public void LoadLevel(string _levelName)
+	{
+		SceneManager.LoadScene(_levelName);
+	}
+
+	public void LoadNextLevel()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1, LoadSceneMode.Single);
+	}
+
+	public void Start()
+	{
+		if (SceneManager.GetActiveScene().buildIndex == 0)
+		{
+			Invoke("LoadNextLevel", 5.0f);
+		}
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioClip[] m_LevelMusicArray;
+	private AudioSource p_AudioSource;
+
+	private void OnLevelWasLoaded(int level)
+	{	
+		p_AudioSource.clip = m_LevelMusicArray[level];
+		p_AudioSource.loop = true;
+		p_AudioSource.Play();
+	}
+
+	private void Awake()
+	{
+		p_AudioSource = GetComponent<AudioSource>() as AudioSource;
+		GameObject.DontDestroyOnLoad(gameObject);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
